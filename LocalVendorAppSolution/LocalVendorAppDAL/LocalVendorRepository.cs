0c9379b149b364f9183f5550220d14d34e1c10e8
Vishal <Vishal@EC2AMAZ-QEF7BJL>
2022-06-02 09:38:11 +0000

    added CUI

--- LocalVendorAppSolution/LocalVendorAppDAL/LocalVendorRepository.cs
+++ LocalVendorAppSolution/LocalVendorAppDAL/LocalVendorRepository.cs
@@ -1,6 +1,8 @@
 ﻿using LocalVendorAppDAL.Models;
+
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,30 @@ namespace LocalVendorAppDAL
 
             return customerList;
         }
+        public int AddSellerUsingUSP(string name,long mobileNumber, string emailId,int shopId, string password )
+        {
+            int result=-1;
+            try
+            {
+                SqlParameter prmName = new SqlParameter("@prmName", name);
+                SqlParameter prmMobileNumber = new SqlParameter("@prmMobileNumber", mobileNumber);
+                SqlParameter prmemailId = new SqlParameter("@email", emailId);
+                SqlParameter prmShopId = new SqlParameter("@id", shopId);
+                SqlParameter prmPassword = new SqlParameter("@password", password);
+                 result = demoAppContext.Database.ExecuteSqlCommand("EXEC dbo.ADDSELLER @prmName,@prmMobileNumber, @email,@id,@password" +
+                    new[] {prmName,prmMobileNumber,prmemailId, prmShopId, prmPassword});    
+
+                
+                             }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return result;
+        }
+
+
 
     }
 }
---
+++ LocalVendorAppSolution/TestConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using LocalVendorAppDAL;
+
+LocalVendorRepository repository = new LocalVendorRepository();
+var  customerDetails= repository.GetCustomerDetails();
+foreach (var item in customerDetails)
+{
+    Console.WriteLine(item.Customerid);
+    Console.WriteLine(item.Name);
+    Console.WriteLine(item.Emailid);
+    Console.WriteLine(item.Mobilenumber);
+
+}
